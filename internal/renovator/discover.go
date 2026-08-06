bc22d22ee04b9fba4b7646f440f76a5c0303d033
d4rkfella <georgi.panov@darkfellanetwork.com>
2026-08-06 12:26:51 +0000

    fear(discovery): improve logging

--- internal/renovator/discover.go
+++ internal/renovator/discover.go
@@ -6,7 +6,10 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
+	"time"
 
+	"github.com/chainguard-dev/clog"
 	"github.com/go-git/go-git/v5"
 	gitconfig "github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
@@ -57,38 +60,81 @@ func repoLocalDir(baseDir string, repo *github.Repository) string {
 }
 
 func prepareRepo(ctx context.Context, repo *github.Repository, token, baseDir string) (localDir string, err error) {
+	log := clog.FromContext(ctx).With(
+		"repository", repo.GetFullName(),
+	)
+
 	localDir = repoLocalDir(baseDir, repo)
-	auth := &githttp.BasicAuth{Username: "x-access-token", Password: token}
+	auth := &githttp.BasicAuth{
+		Username: "x-access-token",
+		Password: token,
+	}
 	defaultBranch := repo.GetDefaultBranch()
 
+	log.Debug(fmt.Sprintf("Initializing git repository into %s", localDir))
+
 	if r, openErr := git.PlainOpen(localDir); openErr == nil {
-		if refreshErr := refreshExistingClone(ctx, r, auth, defaultBranch); refreshErr == nil {
+		log.Debug("Found existing repository")
+
+		start := time.Now()
+
+		refreshErr := refreshExistingClone(ctx, r, auth, defaultBranch)
+		if refreshErr == nil {
+			log.Debug("Repository synchronized",
+				"durationMs", time.Since(start).Milliseconds(),
+			)
+
+			logRepositoryState(log, r)
 			return localDir, nil
 		}
+
+		log.Debug("Failed to refresh repository, removing local clone",
+			"error", refreshErr,
+		)
+
 		_ = os.RemoveAll(localDir)
+	} else {
+		log.Debug("Repository does not exist, creating new shallow clone")
 	}
 
 	if err := os.MkdirAll(filepath.Dir(localDir), 0o755); err != nil {
 		return "", fmt.Errorf("creating repo cache parent dir: %w", err)
 	}
 
-	_, err = git.PlainCloneContext(ctx, localDir, false, &git.CloneOptions{
+	start := time.Now()
+
+	r, err := git.PlainCloneContext(ctx, localDir, false, &git.CloneOptions{
 		URL:          repo.GetCloneURL(),
 		Auth:         auth,
 		Depth:        1,
 		SingleBranch: true,
 	})
+
 	if err != nil {
 		return "", fmt.Errorf("cloning %s: %w", repo.GetFullName(), err)
 	}
+
+	log.Debug("git clone completed",
+		"durationMs", time.Since(start).Milliseconds(),
+	)
+
+	logRepositoryState(log, r)
 
 	return localDir, nil
 }
 
 func refreshExistingClone(ctx context.Context, r *git.Repository, auth *githttp.BasicAuth, defaultBranch string) error {
+	log := clog.FromContext(ctx)
+
 	refSpec := gitconfig.RefSpec(fmt.Sprintf(
-		"+refs/heads/%s:refs/remotes/origin/%s", defaultBranch, defaultBranch,
+		"+refs/heads/%s:refs/remotes/origin/%s",
+		defaultBranch,
+		defaultBranch,
 	))
+
+	log.Debug(fmt.Sprintf("Fetching latest changes from origin/%s", defaultBranch))
+
+	start := time.Now()
 
 	err := r.FetchContext(ctx, &git.FetchOptions{
 		RemoteName: "origin",
@@ -97,27 +143,65 @@ func refreshExistingClone(ctx context.Context, r *git.Repository, auth *githttp.
 		Depth:      1,
 		Force:      true,
 	})
+
 	if err != nil && err != git.NoErrAlreadyUpToDate {
 		return fmt.Errorf("fetching latest %s: %w", defaultBranch, err)
 	}
+
+	log.Debug("git fetch completed",
+		"durationMs", time.Since(start).Milliseconds(),
+	)
 
 	remoteRef, err := r.Reference(plumbing.NewRemoteReferenceName("origin", defaultBranch), true)
 	if err != nil {
 		return fmt.Errorf("resolving fetched ref: %w", err)
 	}
 
+	log.Debug(fmt.Sprintf("Resetting worktree to origin/%s", defaultBranch))
+
 	wt, err := r.Worktree()
 	if err != nil {
 		return fmt.Errorf("getting worktree: %w", err)
 	}
 
-	if err := wt.Reset(&git.ResetOptions{Commit: remoteRef.Hash(), Mode: git.HardReset}); err != nil {
+	if err := wt.Reset(&git.ResetOptions{
+		Commit: remoteRef.Hash(),
+		Mode:   git.HardReset,
+	}); err != nil {
 		return fmt.Errorf("resetting worktree: %w", err)
 	}
 
-	if err := wt.Clean(&git.CleanOptions{Dir: true}); err != nil {
+	log.Debug("Cleaning worktree")
+
+	if err := wt.Clean(&git.CleanOptions{
+		Dir: true,
+	}); err != nil {
 		return fmt.Errorf("cleaning worktree: %w", err)
 	}
 
 	return nil
+}
+
+func logRepositoryState(log *clog.Logger, r *git.Repository) {
+	head, err := r.Head()
+	if err != nil {
+		return
+	}
+
+	commit, err := r.CommitObject(head.Hash())
+	if err != nil {
+		return
+	}
+
+	log.Debug("latest repository commit",
+		"latestCommit", map[string]any{
+			"hash":         commit.Hash.String(),
+			"date":         commit.Author.When,
+			"message":      strings.TrimSpace(commit.Message),
+			"author_name":  commit.Author.Name,
+			"author_email": commit.Author.Email,
+		},
+	)
+
+	log.Debug(fmt.Sprintf("Current branch SHA: %s", head.Hash()))
 }
